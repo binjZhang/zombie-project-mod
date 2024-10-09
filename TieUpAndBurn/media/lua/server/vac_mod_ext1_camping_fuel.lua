feecd0a6e803841dbe79984f631dedd434ad594e
zhangbingjun <zhangbingjun@jkstack.com>
2024-10-09 13:50:00 +0800

    damangedClothes can be lighted

--- TieUpAndBurn/media/lua/server/vac_mod_ext1_camping_fuel.lua
+++ TieUpAndBurn/media/lua/server/vac_mod_ext1_camping_fuel.lua
@@ -2,4 +2,4 @@ require("Camping/camping_fuel")
 campingFuelType["DamagedClothes"] = 15.0  / 60
 campingFuelType["BundleOfClothes"]=15.0 *50 /60
 -- can not be lighted
---campingLightFireType["VacExt.BundleOfClothes"] = 15 / 60 * 10
+campingLightFireType["VacExt.DamagedClothes"] = 15 / 60
