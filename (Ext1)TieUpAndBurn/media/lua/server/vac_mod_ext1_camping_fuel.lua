9dddbaf0b8a5b38d5607d190023a9d464e678bf1
zhangbingjun <zhangbingjun@jkstack.com>
2024-10-11 16:46:20 +0800

    change file struct and new recipe

--- (Ext1)TieUpAndBurn/media/lua/server/vac_mod_ext1_camping_fuel.lua
+++
@@ -1,5 +0,0 @@
-require("Camping/camping_fuel")
-campingFuelType["DamagedClothes"] = 15.0  / 60
-campingFuelType["BundleOfClothes"]=15.0 *50 /60
--- can not be lighted
-campingLightFireType["VacExt.DamagedClothes"] = 15 / 60
